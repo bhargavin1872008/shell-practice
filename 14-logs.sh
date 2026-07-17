9e6df465defcc1f7010173557426f528a2384b77
bhargavi <bhargavin1872008@gmail.com>
2026-07-17 19:44:15 +0530

    Hello world intiated

---
+++ 14-logs.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+VALIDATE( ){
+    if [ $1 -eq 0 ]
+        then
+            echo -e "Installing $2 is ...$G SUCCESS $N" | tee -a $LOG_FILE
+        else
+            echo -e "Installing $2 is ...$R FAILURE $N" | tee -a $LOG_FILE
+            exit 1
+        fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo -e " $R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+dnf list installed nginx &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo "nginx is not installed... going to install it" | tee -a $LOG_FILE
+    dnf install nginx -y &>>$LOG_FILE
+    VALIDATE $? "nginx"
+    
+else
+    echo "nginx is already installed...Nothing to do" | tee -a $LOG_FILE
+fi
+
+#dnf list installed python
+
+dnf list installed python3.12 &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo "python4 is not installed... going to install it" | tee -a $LOG_FILE
+    dnf install python3.12 -y &>>$LOG_FILE
+    VALIDATE $? "python3.12"
+else
+    echo "python3.12 is already installed...Nothing to do" | tee -a $LOG_FILE
+fi
+
+dnf list installed mysql &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo "mysql is not installed... going to install it" | tee -a $LOG_FILE
+    dnf install mysql -y &>>$LOG_FILE
+    VALIDATE $? "mysql"
+    
+else
+    echo "mysql is already installed...Nothing to do" | tee -a $LOG_FILE
+fi
+
+
