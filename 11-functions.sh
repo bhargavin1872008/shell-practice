ebef7411b66effb8b0375841aeb72050b798124b
bhargavi <bhargavin1872008@gmail.com>
2026-07-17 18:40:28 +0530

    Hello world intiated

--- 11-functions.sh
+++ 11-functions.sh
@@ -8,19 +8,30 @@ then
     exit 1 #give other than 0 upto 127
 else
     echo "You are running with root access"
+
 fi
 
-dnf list installed python
-
-dnf list installed python3
+dnf list installed nginx
 if [ $? -ne 0 ]
+then
+    echo "nginx is not installed... going to install it"
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo "nginx is already installed...Nothing to do"
+fi
+
+#dnf list installed python
+
+#dnf list installed python3
+#if [ $? -ne 0 ]
 then
     echo "python3 is not installed... going to install it"
     dnf install python3 -y
     VALIDATE $? "python3"
 else
     echo "python3 is already installed...Nothing to do"
-fi
+#fi
 
 if [ $? -ne 0 ]
 then
