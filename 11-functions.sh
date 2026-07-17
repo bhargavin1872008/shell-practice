7767038652bdb1bc6cc5db3fefc2d275e9a3af81
bhargavi <bhargavin1872008@gmail.com>
2026-07-17 19:25:47 +0530

    Hello world intiated

--- 11-functions.sh
+++ 11-functions.sh
@@ -2,13 +2,22 @@
 
 USERID=$(id -u)
 
+VALIDATE( ){
+    if [ $1 -eq 0 ]
+        then
+            echo "Installing $2 is ... SUCCESS"
+        else
+            echo "Installing $2 is ... FAILURE"
+            exit 1
+        fi
+}
+
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: Please run this script with root access"
     exit 1 #give other than 0 upto 127
 else
     echo "You are running with root access"
-
 fi
 
 dnf list installed nginx
@@ -16,13 +25,8 @@ if [ $? -ne 0 ]
 then
     echo "nginx is not installed... going to install it"
     dnf install nginx -y
-    if [ $? -eq 0 ]
-    then
-        echo "Installing nginx is ... SUCCESS"
-    else
-        echo "Installing nginx is ... FAILURE"
-        exit 1
-    fi
+    VALIDATE $? "nginx"
+    
 else
     echo "nginx is already installed...Nothing to do"
 fi
@@ -34,32 +38,20 @@ if [ $? -ne 0 ]
 then
     echo "python4 is not installed... going to install it"
     dnf install python3.12 -y
-    dnf install python3.12 -y
-    if [ $? -eq 0 ]
-    then
-        echo "Installing python3.12 is ... SUCCESS"
-    else
-        echo "Installing python3.12 is ... FAILURE"
-        exit 1
-    fi
+    VALIDATE $? "python3.12"
 else
     echo "python3.12 is already installed...Nothing to do"
 fi
 
+dnf list installed mysql
 if [ $? -ne 0 ]
 then
-    echo "python3.12 is not installed... going to install it"
-    dnf install python3.12 -y
-    if [ $? -eq 0 ]
-    then
-        echo "Installing MySQL is ... SUCCESS"
-    else
-        echo "Installing MySQL is ... FAILURE"
-        exit 1
-    fi
+    echo "mysql is not installed... going to install it"
+    dnf install mysql -y
+    VALIDATE $? "mysql"
+    
 else
-        echo "MySQL is already installed...Nothing to do"
+    echo "mysql is already installed...Nothing to do"
 fi
 
 
-dnf list installed java
