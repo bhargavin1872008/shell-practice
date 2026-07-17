a90488a1435176ae23deee191eb7de82197b439c
bhargavi <bhargavin1872008@gmail.com>
2026-07-17 16:38:49 +0530

    Hello world intiated

--- 07-array.sh
+++ 07-array.sh
@@ -2,7 +2,7 @@
 
 MOVIES=("Court" "HIT3" "PUSHPA2" "Thandel")
 
-echo "First Movie: MOVIES[0]"
+echo "First Movie: ${MOVIES[0]}"
 echo "First Movie: ${MOVIES[3]}"
 echo "First Movie: ${MOVIES[4]}"
 
