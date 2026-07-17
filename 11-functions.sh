94a1b2a63529ccf382b13a4a569471fa2605568e
bhargavi <bhargavin1872008@gmail.com>
2026-07-17 18:33:24 +0530

    Hello world intiated

--- 11-functions.sh
+++ 11-functions.sh
@@ -12,19 +12,14 @@ fi
 
 dnf list installed python
 
+dnf list installed python3
 if [ $? -ne 0 ]
 then
-    echo "python is not installed... going to install it"
-    dnf install python -y
-    if [ $? -eq 0 ]
-    then
-        echo "Installing python is ... SUCCESS"
-    else
-        echo "Installing python is ... FAILURE"
-        exit 1
-    fi
+    echo "python3 is not installed... going to install it"
+    dnf install python3 -y
+    VALIDATE $? "python3"
 else
-        echo "python is already installed...Nothing to do"
+    echo "python3 is already installed...Nothing to do"
 fi
 
 if [ $? -ne 0 ]
