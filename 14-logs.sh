341bf7dc49f2c7197ea069b0862bf8834cd9207c
bhargavi <bhargavin1872008@gmail.com>
2026-07-17 19:45:09 +0530

    Hello world intiated

--- 14-logs.sh
+++ 14-logs.sh
@@ -11,6 +11,8 @@ LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
+mkdir -p $LOG_FILE
+
 VALIDATE( ){
     if [ $1 -eq 0 ]
         then
