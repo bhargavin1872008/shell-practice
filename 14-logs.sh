c8d78d61c24a53e579e71a55fd7f355de558825c
bhargavi <bhargavin1872008@gmail.com>
2026-07-17 19:53:32 +0530

    Hello world intiated

--- 14-logs.sh
+++ 14-logs.sh
@@ -11,7 +11,8 @@ LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-mkdir -p $LOG_FILE
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 VALIDATE( ){
     if [ $1 -eq 0 ]
