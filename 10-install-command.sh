0926c03843bab8b3b492b0acc478b86be60fa8fe
bhargavi <bhargavin1872008@gmail.com>
2026-07-17 17:52:35 +0530

    Hello world intiated

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -5,9 +5,27 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: Please run this script with root access"
-    echo "$?"
     exit 1 #give other than 0 upto 127
 else
     echo "You are running with root access"
 fi
+
+dnf installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed... going to install it"
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then
+        echo "Installing MySQL is ... SUCCESS"
+    else
+        echo "Installing MySQL is ... FAILURE"
+        exit 1
+    fi
+else
+        echo "MySQL is already installed...Nothing to do"
+fi
+
+
 
